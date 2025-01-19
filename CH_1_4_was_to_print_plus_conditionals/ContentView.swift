204c7f6ec10260ac501b07d20dc76e5f4e5a9cf2
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-18 20:22:23 -0500

    Button Toggle. Toggle between two image,text combinations displayed in VStack. Challenge first submit

--- CH_1_4_was_to_print_plus_conditionals/ContentView.swift
+++ CH_1_4_was_to_print_plus_conditionals/ContentView.swift
@@ -7,16 +7,47 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
+    @State private var systemImageName = ""
+    @State private var imageTextName = ""
     var body: some View {
+        Spacer()
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: systemImageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(imageTextName)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
         }
+        Spacer()
+        HStack {
+            Button("Press Me!") {
+                if (imageTextName == "") {
+                    imageTextName = "You Are Awesome!"
+                    systemImageName = "hand.thumbsup"
+                } else if (imageTextName == "You Are Awesome!") {
+                    systemImageName = "sun.max.fill"
+                    imageTextName = "You Are Great"
+                } else if (imageTextName == "You Are Great") {
+                    systemImageName = "hand.thumbsup"
+                    imageTextName = "You Are Awesome!"
+                } else {
+                    imageTextName = "You Are Awesome!"
+                    systemImageName = "hand.thumbsup"
+                }
+
+                
+            }
+        }
+        .buttonStyle(.borderedProminent)
+        .tint(.orange)
+        .font(.title2)
         .padding()
-    }
+     }
 }
 
 #Preview {
