d914358e39c93bbdde46544b7c91989391ebe47a
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-18 19:10:22 -0500

    Initial Commit

---
+++ CH_1_4_was_to_print_plus_conditionals/CH_1_4_was_to_print_plus_conditionalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CH_1_4_was_to_print_plus_conditionalsApp.swift
+//  CH_1_4_was_to_print_plus_conditionals
+//
+//  Created by Kevin Lefebvre on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CH_1_4_was_to_print_plus_conditionalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CH_1_4_was_to_print_plus_conditionals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CH_1_4_was_to_print_plus_conditionals
+//
+//  Created by Kevin Lefebvre on 1/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
